4095f4b4339a0861ee2dd32340a2568e911b8271
Pratyush Thakur <pratyushthakur7037@gmail.com>
2025-08-09 04:45:37 +0530

    Added .env.example

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ io.on("connection", (socket) => {
 });
 
 // Start server with Socket.IO support
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
