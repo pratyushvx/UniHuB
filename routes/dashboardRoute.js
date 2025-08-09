ade73667b978912e5502096e66e9bc6afda380a3
Pratyush Thakur <pratyushthakur7037@gmail.com>
2025-08-09 22:49:10 +0530

    final

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -3,6 +3,6 @@ import { getDashboard } from "../controller/dashboardController.js";
 
 const router = express.Router();
 
-router.get("/dashboard", getDashboard);
+router.get("/", getDashboard);
 
 export default router;
